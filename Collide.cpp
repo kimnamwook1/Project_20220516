d3345674d82745a7f629182a8ef5e13e7df5018f
KimNamWook <7922378@naver.com>
2022-05-23 16:34:07 +0900

    revise

--- Collide.cpp
+++ Collide.cpp
@@ -5,22 +5,21 @@
 
 Collide::Collide()
 {
-
+	bool IsCollide(const Vector2D& PredictPlayerPosition, Vector2D& OutPlayerPosition)
+	{
+		if (Map[PredictPlayerPosition.Y][PredictPlayerPosition.X] == '#')
+		{
+			return false;
+		}
+		else
+		{
+			OutPlayerPosition = PredictPlayerPosition;
+			return true;
+		}
+	}
 }
 
 Collide::~Collide()
 {
 }
 
-bool IsCollide(const Vector2D& PredictPlayerPosition, Vector2D& OutPlayerPosition)
-{
-	if (Map[PredictPlayerPosition.Y][PredictPlayerPosition.X] == '#')
-	{
-		return false;
-	}
-	else
-	{
-		OutPlayerPosition = PredictPlayerPosition;
-		return true;
-	}
-}
